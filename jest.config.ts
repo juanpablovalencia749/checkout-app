02350b7354c999ac0ad6685f6b8e7aca9aee15cd
ZV <juanpablovalencia749@gmail.com>
2026-02-27 14:51:23 -0500

    feat: initial project setup and install core dependencies
    - Initialize project structure
    - Install Redux Toolkit
    - Install Axios
    - Configure TailwindCSS
    - Setup shadcn/ui
    - Install react-hook-form and zod
    - Configure testing environment (Jest/Vitest)

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+}
+
+export default config
